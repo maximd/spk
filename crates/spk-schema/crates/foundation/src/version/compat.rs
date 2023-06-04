8cda6cd0cd01b3d8506875ecb245afb3bb5b6518
J Robert Ray <jrray@jrray.org>
2023-06-04 16:08:58 -0700

    Dedicated incompatibility type for missing embedded components
    Build on the ComponentsMissing type to be able to detect when missing
    components are from some package not embedding them (or requests for them
    are missing).
    
        .. TRY victim/1.0.0/3I42H3S6 - fake-pkg embeds real-pkg but does not provide all required components: needed comp2, have comp1
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-schema/crates/foundation/src/version/compat.rs
+++ crates/spk-schema/crates/foundation/src/version/compat.rs
@@ -99,6 +99,7 @@ pub struct ComponentsMissing {
 pub enum IncompatibleReason {
     ComponentsMissing(ComponentsMissing),
     ConflictingEmbeddedPackage(PkgNameBuf),
+    EmbeddedComponentsMissing(PkgNameBuf, ComponentsMissing),
     Other(String),
 }
 
@@ -131,6 +132,30 @@ impl std::fmt::Display for IncompatibleReason {
                     f,
                     "embedded package conflicts with existing package in solve: {pkg}"
                 )
+            }
+            IncompatibleReason::EmbeddedComponentsMissing(
+                pkg,
+                ComponentsMissing {
+                    package,
+                    provided,
+                    missing,
+                },
+            ) => {
+                write!(f, "{pkg} embeds {package} but does not provide all required components: needed {}, have {}",
+                 missing
+                     .iter()
+                     .map(Component::to_string)
+                     .join("\n"),
+                 {
+                     if provided.is_empty() {
+                         "none".to_owned()
+                     } else {
+                         provided
+                             .iter()
+                             .map(Component::to_string)
+                             .join("\n")
+                     }
+                 })
             }
             IncompatibleReason::Other(msg) => f.write_str(msg),
         }
