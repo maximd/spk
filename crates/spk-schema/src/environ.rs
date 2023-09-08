e8092ba11d835c07f8327be20459115646afb914
Nichol Yip <nyip@imageworks.com>
2023-09-08 11:09:36 -0700

    Reworked NoOp type for EnvOp to UnrecognizedKey.
    Signed-off-by: Nichol Yip <nyip@imageworks.com>
    
    WIP: Adding lint feature to source spec
    
    Signed-off-by: Nichol Yip <nyip@imageworks.com>

--- crates/spk-schema/src/environ.rs
+++ crates/spk-schema/src/environ.rs
@@ -24,6 +24,7 @@ const OP_COMMENT: &str = "comment";
 const OP_PREPEND: &str = "prepend";
 const OP_PRIORITY: &str = "priority";
 const OP_SET: &str = "set";
+const OP_NAMES: &[&str] = &[OP_APPEND, OP_COMMENT, OP_PREPEND, OP_SET];
 
 /// The set of operation types for use in deserialization
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -32,8 +33,8 @@ pub enum OpKind {
     Comment,
     Prepend,
     Priority,
-    NoOp,
     Set,
+    UnrecognizedKey,
 }
 
 /// An operation performed to the environment
@@ -45,7 +46,7 @@ pub enum EnvOp {
     Prepend(PrependEnv),
     Priority(EnvPriority),
     Set(SetEnv),
-    NoOp(NoOp),
+    UnrecognizedKey(UnrecognizedKey),
 }
 
 impl EnvOp {
@@ -56,7 +57,18 @@ impl EnvOp {
             EnvOp::Prepend(_) => OpKind::Prepend,
             EnvOp::Priority(_) => OpKind::Priority,
             EnvOp::Set(_) => OpKind::Set,
-            EnvOp::NoOp(_) => OpKind::NoOp,
+            EnvOp::UnrecognizedKey(_) => OpKind::UnrecognizedKey,
+        }
+    }
+
+    pub fn error(&self) -> &str {
+        match self {
+            EnvOp::Append(_) => "",
+            EnvOp::Comment(_) => "",
+            EnvOp::Prepend(_) => "",
+            EnvOp::Priority(_) => "",
+            EnvOp::Set(_) => "",
+            EnvOp::UnrecognizedKey(e) => e.error.as_str(),
         }
     }
 
@@ -134,7 +146,7 @@ impl EnvOp {
             Self::Prepend(op) => op.bash_source(),
             Self::Priority(op) => op.bash_source(),
             Self::Set(op) => op.bash_source(),
-            Self::NoOp(op) => op.bash_source(),
+            Self::UnrecognizedKey(op) => op.bash_source(),
         }
     }
 
@@ -146,7 +158,7 @@ impl EnvOp {
             Self::Prepend(op) => op.tcsh_source(),
             Self::Priority(op) => op.tcsh_source(),
             Self::Set(op) => op.tcsh_source(),
-            Self::NoOp(op) => op.tcsh_source(),
+            Self::UnrecognizedKey(op) => op.tcsh_source(),
         }
     }
 
@@ -194,31 +206,31 @@ impl Lints for EnvOpVisitor {
 
 impl From<EnvOpVisitor> for EnvOp {
     fn from(mut value: EnvOpVisitor) -> Self {
-        match value.op_and_var.take() {
-            Some((op, var)) => match op {
-                OpKind::Prepend => EnvOp::Prepend(PrependEnv {
-                    prepend: var.get_op(),
-                    separator: value.separator.take(),
-                    value: value.value.expect("an environment value"),
-                }),
-                OpKind::Append => EnvOp::Append(AppendEnv {
-                    append: var.get_op(),
-                    separator: value.separator.take(),
-                    value: value.value.expect("an environment value"),
-                }),
-                OpKind::Set => EnvOp::Set(SetEnv {
-                    set: var.get_op(),
-                    value: value.value.expect("an environment value"),
-                }),
-                OpKind::Comment => EnvOp::Comment(CommentEnv {
-                    comment: var.get_op(),
-                }),
-                OpKind::Priority => EnvOp::Priority(Priority {
-                    priority: var.get_priority(),
-                }),
-                OpKind::NoOp => EnvOp::NoOp(NoOp),
-            },
-            None => EnvOp::NoOp(NoOp),
+        let (op, var) = value.op_and_var.expect("an operation and variable");
+        match op {
+            OpKind::Prepend => EnvOp::Prepend(PrependEnv {
+                prepend: var.get_op(),
+                separator: value.separator.take(),
+                value: value.value.expect("an environment value"),
+            }),
+            OpKind::Append => EnvOp::Append(AppendEnv {
+                append: var.get_op(),
+                separator: value.separator.take(),
+                value: value.value.expect("an environment value"),
+            }),
+            OpKind::Set => EnvOp::Set(SetEnv {
+                set: var.get_op(),
+                value: value.value.expect("an environment value"),
+            }),
+            OpKind::Comment => EnvOp::Comment(CommentEnv {
+                comment: var.get_op(),
+            }),
+            OpKind::Priority => EnvOp::Priority(Priority {
+                priority: var.get_priority(),
+            }),
+            OpKind::UnrecognizedKey => EnvOp::UnrecognizedKey(UnrecognizedKey {
+                error: var.get_op(),
+            }),
         }
     }
 }
@@ -292,9 +304,13 @@ impl<'de> serde::de::Visitor<'de> for EnvOpVisitor {
                 "separator" => {
                     self.separator = map.next_value::<Option<Stringified>>()?.map(|s| s.0)
                 }
-                unknown_config => {
+                unknown_key => {
                     self.lints
-                        .push(LintMessage::UnknownEnvOpKey(EnvOpKey::new(unknown_config)));
+                        .push(LintMessage::UnknownEnvOpKey(EnvOpKey::new(unknown_key)));
+                    self.op_and_var = Some((
+                        OpKind::UnrecognizedKey,
+                        ConfKind::Operation(format!("missing field to define operation and variable, expected one of {OP_NAMES:?}")),
+                    ));
                     map.next_value::<serde::de::IgnoredAny>()?;
                 }
             }
@@ -303,7 +319,7 @@ impl<'de> serde::de::Visitor<'de> for EnvOpVisitor {
         // Comments and priority configs don't have any values.
         let value = match self.op_and_var.as_ref() {
             Some(v) => match v.0 {
-                OpKind::Comment | OpKind::Priority => String::from(""),
+                OpKind::Comment | OpKind::Priority | OpKind::UnrecognizedKey => String::from(""),
                 _ => self
                     .value
                     .take()
@@ -477,15 +493,18 @@ impl SetEnv {
     }
 }
 
+/// Stores the error message of the unrecognized key
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
-pub struct NoOp;
+pub struct UnrecognizedKey {
+    pub error: String,
+}
 
-/// Returns empty string from methods when the operation is not found in deserialize
-impl NoOp {
+impl UnrecognizedKey {
+    /// Empty bash source
     pub fn bash_source(&self) -> String {
         String::from("")
     }
-
+    /// Empty tcsh source
     pub fn tcsh_source(&self) -> String {
         String::from("")
     }
